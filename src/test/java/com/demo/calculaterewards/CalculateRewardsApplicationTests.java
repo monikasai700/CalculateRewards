c81e7ae92c5c57bc0dea3d5ad9f3695f00ff986f
monikasai700 <monikasai700@gmail.com>
2023-02-15 17:18:04 -0500

    ST-initial commit for calculate rewards

---
+++ src/test/java/com/demo/calculaterewards/CalculateRewardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.calculaterewards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculateRewardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
